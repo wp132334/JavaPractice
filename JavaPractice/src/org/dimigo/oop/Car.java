4acead82cac9ce623af79ee7d7cbd4d9cdc04574
산결 <@Baby_Tiragon>
2015-04-14 11:54:11 +0900

    과제 제출

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,49 @@
+package org.dimigo.oop;
+
+public class Car {
+	public String company;
+	public String model;
+	public String color;
+	public int maxSpeed;
+	public int price;
+	
+	public String getCompany() {
+		return company;
+	}
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setCompany(String Company) {
+		company = Company;
+	}
+	
+	public void setModel(String Model) {
+		model = Model;
+	}
+	
+	public void setColor(String Color) {
+		color = Color;
+	}
+	
+	public void setMaxSpeed(int MaxSpeed) {
+		maxSpeed = MaxSpeed;
+	}
+	
+	public void setPrice(int Price) {
+		price = Price;
+	}
+}
